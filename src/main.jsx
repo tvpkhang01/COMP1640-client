29040dbd9d648ee19333e0cb8239492277bc8184
HoangLoc <huynhngochoangloc0211102@gmail.com>
2024-03-04 12:37:16 +0700

    setup code

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import axios from  "axios";
 
+const BASE_URL = import.meta.env.VITE_BASE_URL_API;
+axios.defaults.baseURL = BASE_URL;
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
