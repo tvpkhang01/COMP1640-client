f17494737e441476a3f360610af5bf92ba832907
thaidq <thaidqgcd19963@fpt.edu.vn>
2024-03-11 11:07:27 +0700

    OT1-9: Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,80 @@
+import "./Header.css"
+import { Dropdown, Menu, Tooltip } from 'antd';
+import {
+  SearchOutlined,
+  HomeOutlined,
+  PlusSquareOutlined,
+  UserOutlined,
+  UnorderedListOutlined,
+} from '@ant-design/icons';
 
+const menuItems = [
+  'Giao diện',
+  'Cài đặt',
+  'Đã lưu',
+  'Lượt thích của bạn',
+  'Báo cáo sự cố',
+  'Đăng xuất',
+];
+
+const dropdownMenu = (
+  <Menu>
+    {menuItems.map((item, index) => (
+      <Menu.Item key={index}>
+        <a href="#">{item}</a>
+      </Menu.Item>
+    ))}
+  </Menu>
+);
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='Container'>
+      <div className="Left">
+        <span className="Logo">
+          <img src="https://res.cloudinary.com/detqcm2nt/image/upload/v1709907116/samples/threads.jpg" alt="" />
+        </span>
+      </div>
+
+      <div className="Center">
+        <Tooltip title="Home">
+          <a href="1">
+            <div className="Home">
+              <HomeOutlined />
+            </div>
+          </a>
+        </Tooltip>
+        <Tooltip title="Search">
+          <a href="2">
+            <div className="Search">
+              <SearchOutlined />
+            </div>
+          </a>
+        </Tooltip>
+        <Tooltip title="Create">
+          <a href="3">
+            <div className="More">
+              <PlusSquareOutlined />
+            </div>
+          </a>
+        </Tooltip>
+        <Tooltip title="Profile">
+          <a href="4">
+            <div className="Person">
+              <UserOutlined />
+            </div>
+          </a>
+        </Tooltip>
+      </div>
+
+      <div className="Right">
+        <Dropdown overlay={dropdownMenu} trigger={['click']}>
+          <span>
+            <UnorderedListOutlined />
+          </span>
+        </Dropdown>
+      </div>
+    </div>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,79 @@
+import { useRef, useState } from "react";
+import { Modal, Form, Button } from "antd";
 import "./Home.css"
-import React from "react"
+import {
+  AreaChartOutlined,
+  FileAddOutlined
+} from '@ant-design/icons';
 
 function Home() {
+  const desc = useRef()
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [setPhoto] = useState(null)
+  const [setFile] = useState(null)
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
   return (
-    <div>Home</div>
+    <div className="share">
+      <div className="shareWrapper">
+        <div className="shares">
+          <img className= "shareProfile" src="https://res.cloudinary.com/detqcm2nt/image/upload/v1709743558/avatar.jpg" alt="" />
+          <input 
+            placeholder=""
+            className="shareInput"
+            ref={desc}
+            onClick={showModal}
+          />
+          <button className="shareButton">Post</button>
+          <Modal
+            title="COMP1640 new"
+            visible={isModalVisible}
+            onCancel={handleCancel}
+            width="600px"
+            id="modal-content"
+            footer={[
+              <Button  className="shareButton" key="post" type="darkgray" id="modal-button-post" onClick={handleCancel}>
+                Post
+              </Button>,
+          ]}>
+            <Form>
+              <div className="shareWrapper">
+                <div className="shares">
+                  <img className= "shareProfile" src="https://res.cloudinary.com/detqcm2nt/image/upload/v1709743558/avatar.jpg" alt="" />
+                  <input 
+                    placeholder="what's in your mind?"
+                    className="shareInput"
+                    ref={desc}
+                  />
+                </div>
+                <hr className="shareHr" />
+                <form className="shareBottom">
+                  <div className="shareOptions">
+                    <label htmlFor="file" className="shareOption">
+                      <AreaChartOutlined className="shareIcon"/>
+                      <span className="shareOptionText">Photo or Video</span>
+                      <input style={{display: "none"}} type="photo" id="photo" accept=".png, .jpeg" onChange={(e)=>setPhoto(e.target.files[0])}/>
+                    </label>
+                    <label htmlFor="file" className="shareOption">
+                      <FileAddOutlined />
+                      <span className="shareOptionText">File</span>
+                      <input style={{display: "none"}} type="file" id="file" accept=".doc, .pdf" onChange={(e)=>setFile(e.target.files[0])}/>
+                    </label>
+                  </div>
+                </form>
+              </div>
+            </Form>
+          </Modal>
+        </div>
+      </div>
+    </div>
   )
 }
 
