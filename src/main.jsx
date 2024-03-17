3b040774be05833fd8be2d96d6c6b1c9a487c219
HoangLoc <huynhngochoangloc0211102@gmail.com>
2024-03-17 21:58:55 +0700

    improve(OT1-60): Change to React

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import React, {Suspense} from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from "react-router-dom";
-import axios from  "axios";
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom';
+import { RouterProvider } from 'react-router-dom';
+import axios from 'axios';
 import Router from './routers/Router';
 
-
-axios.defaults.baseURL = "http://localhost:3000/";
+axios.defaults.baseURL = 'http://localhost:3000/';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Suspense>
-      <RouterProvider router={ Router } />
+    <Suspense fallback={<div>Loading...</div>}>
+      <RouterProvider router={Router} />
     </Suspense>
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
