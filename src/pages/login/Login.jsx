257278a855c477aaa4d2b9f516a6d6c2817bec93
HuynhNgocHoangLoc1 <129734268+HuynhNgocHoangLoc1@users.noreply.github.com>
2024-03-18 11:30:08 +0700

    OT1-60 (#6)
    * improve(OT1-60): Change to React
    
    * feat(OT1-60): Responsive LoginPage

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,46 +1,125 @@
 import React, { useState } from 'react';
+import { Row, Col, Typography, Input, Button, Space } from 'antd';
 import './LoginPage.css';
 
+const { Title, Text } = Typography;
+
 function LoginPage() {
-  const backgroundImageURL = 'https://res.cloudinary.com/dh6dvndzn/image/upload/v1709633495/imgLogin.png';
-
-
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-
-
-  const handleRoleChange = (event) => {
-    setRole(event.target.value);
-  };
-
   const handleLogin = () => {
-    // Xử lý đăng nhập, có thể sử dụng các giá trị trong state như name, email, password, mssv, role
+    // Xử lý đăng nhập ở đây
   };
 
-
-const handleGuestLogin = () => {
-  // Xử lý đăng nhập với tài khoản khách ở đây
-};
+  const handleGuestLogin = () => {
+    // Xử lý đăng nhập với tài khoản khách ở đây
+  };
 
   return (
-    <div className="LoginPage">
-      <div className="LoginPage background-image">
-</div>
-      <h1>Login with account google</h1>
-      
-      <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email" />
-      <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter your password" />
-   
-      <button onClick={handleLogin}>Login</button>
+    <Row className="Row" style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+      <Col span={24} style={{ textAlign: 'center' }}>
+        <Row className="background-image"></Row>
+        <Title level={2}>Login with account google</Title>
 
-      <div className="line"></div>
+        <Row gutter={[0, 10]} className="Row">
+          <Col span={24} style={{ textAlign: 'center', marginBottom: '1px' }}>
+            <Input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter your Email"
+              style={{ width: '100%', maxWidth: '350px', padding: '15px', marginBottom: '10px', boxSizing: 'border-box', border: '1px solid #ccc', borderRadius: '10px', opacity: '0.8' }}
+            />
+          </Col>
+          
+          <Col span={24} style={{ textAlign: 'center', marginBottom: '-10px', marginTop: '-10px' }}>
+            <Input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter your Password"
+              style={{ width: '100%', maxWidth: '350px', padding: '15px', marginBottom: '10px', boxSizing: 'border-box', border: '1px solid #ccc', borderRadius: '10px', opacity: '0.8' }}
+            />
+          </Col>
 
-      
-      <button className="login-with-guest" onClick={handleGuestLogin}>Login with Guest Account</button>
+          <Col span={24} style={{ marginBottom: '2px', textAlign: 'center', position: 'relative' }}>
+            <Button
+              type="primary"
+              onClick={handleLogin}
+              style={{
+                width: '100%',
+                maxWidth: '350px',
+                padding: '20px',
+                marginBottom: '20px',
+                boxSizing: 'border-box',
+                backgroundColor: 'black',
+                color: 'white',
+                border: 'none',
+                borderRadius: '10px',
+                cursor: 'pointer',
+                display: 'inline-flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                textAlign: 'center',
+                position: 'relative',
+                marginBottom: '10px',
 
-      
-    </div>
+              }}
+            >
+              Login
+              
+            </Button>
+          
+          </Col>
+          <Col span={24} style={{ marginBottom: '2px', textAlign: 'center', marginTop: '-10px'}}>
+            <Space direction="horizontal">
+            <a href="#" className="forgot-password" style={{color: 'black'}}>Forgot Password?</a>
+            </Space>
+            <div
+      style={{
+        position: 'absolute',
+        width: '24%',
+        borderBottom: '2px solid rgba(0, 0, 0, 0.3)',
+        bottom: '-20px',
+        left: '50%',
+        transform: 'translateX(-50%)', 
+      }}
+    ></div>
+          </Col>
+          <Col span={24} style={{ marginBottom: '2px', textAlign: 'center', position: 'relative' }}>
+            <Button
+              type="default"
+              className="login-with-guest"
+              onClick={handleGuestLogin}
+              style={{
+                width: '100%',
+                maxWidth: '350px',
+                padding: '20px',
+                top: '30px',
+                marginBottom: '20px',
+                boxSizing: 'border-box',
+                backgroundColor: 'black',
+                color: 'white',
+                border: 'none',
+                borderRadius: '10px',
+                cursor: 'pointer',
+                display: 'inline-flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                textAlign: 'center',
+                position: 'relative',
+              }}
+            >
+              Login with Guest Account
+              
+            </Button>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
   );
 }
 
