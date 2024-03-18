be85df0806b24b52e0602fc89734eaeeb8281c35
tientvgcd201862 <tientvgcd201862@fpt.edu.vn>
2024-03-18 20:00:23 +0700

    feat(OT1-59): Create Admin Page

--- src/pages/admin/AdminHome.jsx
+++ src/pages/admin/AdminHome.jsx
@@ -1,8 +1,20 @@
+import { Typography } from 'antd';
+import { Link } from 'react-router-dom';
+
+const { Title, Text } = Typography;
 
 function AdminHome() {
   return (
-    <div>AdminHome</div>
-  )
+    <>
+      <Title level={1} style={{ textAlign: 'center', fontSize: '32px' }}>Hello Admin</Title>
+      <Typography.Paragraph style={{ textAlign: 'center', fontSize: '18px' }}>
+        You can manage <Link to="/admin/user" style={{ fontWeight: 'bold', textDecoration: 'underline', color: 'black' }}>user</Link> or <Link to="/admin/semester" style={{ fontWeight: 'bold', textDecoration: 'underline', color: 'black' }}>semester</Link> now.
+      </Typography.Paragraph>
+      <Typography.Paragraph style={{ textAlign: 'center' }}>
+        <img src="https://res.cloudinary.com/dxvlo5gta/image/upload/v1710751368/admin.jpg" alt="Admin" style={{ display: 'block', margin: 'auto' }} />
+      </Typography.Paragraph>
+    </>
+  );
 }
 
-export default AdminHome
+export default AdminHome;
