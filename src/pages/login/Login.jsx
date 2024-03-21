f4974284b3a60dd5c143745fc1072c009996b5dd
HoangLoc <huynhngochoangloc0211102@gmail.com>
2024-03-21 16:44:55 +0700

    improve(OT1-68): delete line and button login with guest account

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -20,7 +20,7 @@ function LoginPage() {
     <Row className="Row" style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
       <Col span={24} style={{ textAlign: 'center' }}>
         <Row className="background-image"></Row>
-        <Title level={2}>Login with account google</Title>
+        <Title level={2}>Login with your account</Title>
 
         <Row gutter={[0, 10]} className="Row">
           <Col span={24} style={{ textAlign: 'center', marginBottom: '1px' }}>
@@ -74,49 +74,8 @@ function LoginPage() {
             </Button>
           
           </Col>
-          <Col span={24} style={{ marginBottom: '2px', textAlign: 'center', marginTop: '-10px'}}>
-            <Space direction="horizontal">
-            <a href="#" className="forgot-password" style={{color: 'black'}}>Forgot Password?</a>
-            </Space>
-            <div
-      style={{
-        position: 'absolute',
-        width: '24%',
-        borderBottom: '2px solid rgba(0, 0, 0, 0.3)',
-        bottom: '-20px',
-        left: '50%',
-        transform: 'translateX(-50%)', 
-      }}
-    ></div>
-          </Col>
-          <Col span={24} style={{ marginBottom: '2px', textAlign: 'center', position: 'relative' }}>
-            <Button
-              type="default"
-              className="login-with-guest"
-              onClick={handleGuestLogin}
-              style={{
-                width: '100%',
-                maxWidth: '350px',
-                padding: '20px',
-                top: '30px',
-                marginBottom: '20px',
-                boxSizing: 'border-box',
-                backgroundColor: 'black',
-                color: 'white',
-                border: 'none',
-                borderRadius: '10px',
-                cursor: 'pointer',
-                display: 'inline-flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                textAlign: 'center',
-                position: 'relative',
-              }}
-            >
-              Login with Guest Account
-              
-            </Button>
-          </Col>
+          
+          
         </Row>
       </Col>
     </Row>
