d081aff05cfbb49ceab2b68f4ac669e4a2d2ba56
thaidq <thaidqgcd19963@fpt.edu.vn>
2024-03-23 20:30:06 +0700

    fead(OT1-44): done account management fix

--- src/pages/admin/user/ReadUser.jsx
+++ src/pages/admin/user/ReadUser.jsx
@@ -1,56 +1,95 @@
 import React, { useState } from 'react';
-import { Input, Button, Flex, Form, Space, Modal, Select, Row, Col, Avatar, Card, Descriptions } from 'antd';
+import { Input, Button, Form, Modal, Select, Row, Col, Card, Descriptions, Divider, Flex } from 'antd';
 import { SearchOutlined, CloseCircleFilled, DeleteOutlined, EditOutlined } from '@ant-design/icons';
-import './ReadUser.css'; // Import CSS file for custom styles
+import './ReadUser.css';
+
+const { Meta } = Card;
 
 function ReadUser() {
   const [searchValue, setSearchValue] = useState('');
-  const [showClearIcon, setShowClearIcon] = useState(false);
   const [selectedButton, setSelectedButton] = useState(null);
+  const [showClearIcon, setShowClearIcon] = useState(false);
   const [modalVisible, setModalVisible] = useState(false);
   const [newRole, setNewRole] = useState("");
+  const [selectedRole, setSelectedRole] = useState("");
   const [newFacuty, setNewFacuty] = useState("");
   const [form] = Form.useForm();
-  const { Meta } = Card;
-  const [cards, setCards] = useState([]); // State to store card data
+  const [cards, setCards] = useState([]);
   const [editIndex, setEditIndex] = useState(null);
-
 
   const handleSearch = (value) => {
     setSearchValue(value);
-    setShowClearIcon(!!value); // Hiển thị biểu tượng khi có nội dung trong ô input
-  };
-
-  const handleClearInput = () => {
-    setSearchValue('');
-    setShowClearIcon(false); // Ẩn biểu tượng khi xóa nội dung của ô input
+    setShowClearIcon(!!value);
   };
 
   const handleButtonClick = (buttonId) => {
     setSelectedButton(buttonId);
   };
 
+  const handleClearInput = () => {
+    setSearchValue('');
+    setShowClearIcon(false);
+  };
+
   const handleModalOpen = () => {
     setModalVisible(true);
-    setNewRole("");
-    setNewFacuty("");
+  };
+
+  const handleFormSubmit = () => {
+    form.validateFields().then((values) => {
+      const { fullname, gmail } = values;
+      const isDuplicate = cards.some((card, index) => {
+        if (editIndex !== null && editIndex !== undefined && index === editIndex) {
+          return false;
+        }
+        return card.fullname === fullname || card.gmail === gmail;
+      });
+  
+      if (isDuplicate) {
+        Modal.error({
+          title: 'Error',
+          content: 'Fullname or Gmail already exists!',
+        });
+      } else {
+        if (editIndex !== null && editIndex !== undefined) {
+          const updatedCards = [...cards];
+          updatedCards[editIndex] = { ...values };
+          setCards(updatedCards);
+          setEditIndex(null); // Đặt lại giá trị editIndex thành null
+        } else {
+          const isEditMode = editIndex !== null && editIndex !== undefined;
+          if (isEditMode) {
+            Modal.error({
+              title: 'Error',
+              content: 'Cannot create new card while editing an existing one!',
+            });
+          } else {
+            const newCard = { ...values };
+            setCards([...cards, newCard]);
+          }
+        }
+        console.log('Form submitted:', values);
+        handleModalClose();
+      }
+    });
   };
 
   const handleModalClose = () => {
     setModalVisible(false);
+    form.resetFields();
   };
 
-  const handleFormSubmit = (values) => {
-    const newCard = { ...values };
-    setCards([...cards, newCard]); // Add new card data to the existing cards
-    console.log('Form submitted:', newCard);
-    // Đóng Modal sau khi form được gửi đi
-    handleModalClose();
+  const handleChangeRole = (value) => {
+    setSelectedRole(value);
+    setNewRole(value);
+    if (value === "Mm") {
+      setNewFacuty("");
+    }
   };
 
   const handleEdit = (index) => {
-    setEditIndex(index); // Cập nhật chỉ số của card đang được chỉnh sửa
-    form.setFieldsValue(cards[index]); // Cập nhật dữ liệu trong form với dữ liệu của card được chọn
+    setEditIndex(index);
+    form.setFieldsValue(cards[index]);
     handleModalOpen();
   };
 
@@ -63,67 +102,68 @@ function ReadUser() {
   return (
     <>
       <Form className="input-container" onFinish={() => {}}>
-          <Input 
-            className='custom-input'
-            prefix={<SearchOutlined />}
-            suffix=
-              {
-                showClearIcon && (
-                  <CloseCircleFilled
-                    style={{ fontSize: '15px', cursor: 'pointer', margin:'10px' }}
-                    onClick={handleClearInput}
-                  />
-                )
-              }
-            placeholder="Search"
-            value={searchValue}
-            onChange={(e) => handleSearch(e.target.value)}
-          />
+        <Input 
+          className='custom-input'
+          prefix={<SearchOutlined />}
+          suffix={
+            showClearIcon && (
+              <CloseCircleFilled
+                style={{ fontSize: '15px', cursor: 'pointer', margin:'10px' }}
+                onClick={handleClearInput}
+              />
+            )
+          }
+          placeholder="Search"
+          value={searchValue}
+          onChange={(e) => handleSearch(e.target.value)}
+        />
       </Form>
 
-      <Flex className='custom-flex'>
-        <Button
-          className={selectedButton === 'all' ? 'selected' : null}
-          onClick={() => handleButtonClick('all')}
-        >
-          All
-        </Button>
-        <Button
-          className={selectedButton === 'follow' ? 'selected' : null}
-          onClick={() => handleButtonClick('follow')}
-        >
-          Follow
-        </Button>
-        <Button
-          className={selectedButton === 'replies' ? 'selected' : null}
-          onClick={() => handleButtonClick('replies')}
-        >
-          Replies
-        </Button>
-        <Button
-          className={selectedButton === 'mentions' ? 'selected' : null}
-          onClick={() => handleButtonClick('mentions')}
-        >
-          Mentions
-        </Button>
-        <Button
-          className={selectedButton === 'quotes' ? 'selected' : null}
-          onClick={() => handleButtonClick('quotes')}
-        >
-          Quotes
-        </Button>
-        <Button
-          className={selectedButton === 'reposts' ? 'selected' : null}
-          onClick={() => handleButtonClick('reposts')}
-        >
-          Reposts
-        </Button>
-        <Button
-          className={selectedButton === 'verified' ? 'selected' : null}
-          onClick={() => handleButtonClick('verified')}
-        >
-          Verified
-        </Button>
+      <Flex className="custom-flex-container" >
+        <Flex className='custom-flex'>
+          <Button
+            className={selectedButton === 'all' ? 'selected' : null}
+            onClick={() => handleButtonClick('all')}
+          >
+            All
+          </Button>
+          <Button
+            className={selectedButton === 'follow' ? 'selected' : null}
+            onClick={() => handleButtonClick('follow')}
+          >
+            Follow
+          </Button>
+          <Button
+            className={selectedButton === 'replies' ? 'selected' : null}
+            onClick={() => handleButtonClick('replies')}
+          >
+            Replies
+          </Button>
+          <Button
+            className={selectedButton === 'mentions' ? 'selected' : null}
+            onClick={() => handleButtonClick('mentions')}
+          >
+            Mentions
+          </Button>
+          <Button
+            className={selectedButton === 'quotes' ? 'selected' : null}
+            onClick={() => handleButtonClick('quotes')}
+          >
+            Quotes
+          </Button>
+          <Button
+            className={selectedButton === 'reposts' ? 'selected' : null}
+            onClick={() => handleButtonClick('reposts')}
+          >
+            Reposts
+          </Button>
+          <Button
+            className={selectedButton === 'verified' ? 'selected' : null}
+            onClick={() => handleButtonClick('verified')}
+          >
+            Verified
+          </Button>
+        </Flex>
       </Flex>
 
       <Form form={form} onFinish={handleFormSubmit}>
@@ -140,7 +180,7 @@ function ReadUser() {
 
         <Modal
           title="Add User"
-          visible={modalVisible}
+          open={modalVisible}
           onCancel={handleModalClose}
           footer={null}
           centered
@@ -149,12 +189,23 @@ function ReadUser() {
             className="modal-form"
             onFinish={handleFormSubmit}
             layout="vertical"
+            form={form}
           >
-            <Form.Item label="Full name" name="fullname" >
+            <Form.Item label="Full name" name="fullname" rules={[{ required: true, message: 'Please input your full name!' }]}>
               <Input />
             </Form.Item>
 
-            <Form.Item label="Gmail" name="gmail" >
+            <Form.Item
+              label="Gmail"
+              name="gmail"
+              rules={[
+                { required: true, message: 'Please input your Gmail!' },
+                {
+                  pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: 'Please enter a valid email address!',
+                },
+              ]}
+            >
               <Input />
             </Form.Item>
 
@@ -163,14 +214,9 @@ function ReadUser() {
                 <Form.Item label="Role" name="role">
                   <Select
                     value={newRole}
-                    onChange={(selectedValues) => setNewRole(selectedValues)}
+                    onChange={handleChangeRole}
                   >
-                    {[
-                      "Admin",
-                      "Student",
-                      "Mm",
-                      "Mc"
-                    ].map((option) => (
+                    {["Student", "Mm", "Mc"].map((option) => (
                       <Select.Option key={option} value={option}>
                         {option}
                       </Select.Option>
@@ -184,8 +230,9 @@ function ReadUser() {
                   <Select
                     value={newFacuty}
                     onChange={(selectedValues) => setNewFacuty(selectedValues)}
+                    disabled={selectedRole === "Mm"}
                   >
-                    {[
+                    {[ 
                       "IT",
                       "Business",
                       "Graphic Design",
@@ -199,10 +246,6 @@ function ReadUser() {
               </Col>
             </Row>
 
-            {/* <Form.Item label="Birthdate" name="birthdate">
-              <DatePicker />
-            </Form.Item> */}
-
             <Form.Item wrapperCol={{ offset: 10, span: 16 }}>
               <Button type="primary" htmlType="submit">
                 Submit
@@ -214,37 +257,31 @@ function ReadUser() {
       </Form>
 
       {cards.map((card, index) => (
-        <Card
-          className='data-user'
-          key={index}
-          style={{
-            width: 600,
-            marginTop: 16,
-            margin: 'auto',
-          }}
-          actions={[
-            <EditOutlined key="edit" onClick={() => handleEdit(index)} />,
-            <DeleteOutlined key="delete" onClick={() => handleDelete(index)} />,
-          ]}
-        >
-          <Row gutter={16}>
-            <Col span={12}>
-              <Descriptions column={1}>
-                <Descriptions.Item label="Full name">{card.fullname}</Descriptions.Item>
-                <Descriptions.Item label="Gmail">{card.gmail}</Descriptions.Item>
-              </Descriptions>
-            </Col>
-          
-            <Col span={12}>
-              <Descriptions column={1}>
-                <Descriptions.Item label="Role">{card.role}</Descriptions.Item>
-                <Descriptions.Item label="Faculty">{card.faculty}</Descriptions.Item>
-              </Descriptions>
-            </Col>
-          </Row>
-        </Card>
+        <React.Fragment key={index}>
+          <Card
+            actions={[
+              <EditOutlined key="edit" onClick={() => handleEdit(index)} />,
+              <DeleteOutlined key="delete" onClick={() => handleDelete(index)} />,
+            ]}
+          >
+            <Row gutter={16} style={{ borderColor: 'black' }}>
+              <Col span={12}>
+                <Descriptions column={1}>
+                  <Descriptions.Item label="Full name">{card.fullname}</Descriptions.Item>
+                  <Descriptions.Item label="Gmail">{card.gmail}</Descriptions.Item>
+                </Descriptions>
+              </Col>
+              <Col span={12}>
+                <Descriptions column={1}>
+                  <Descriptions.Item label="Role">{card.role}</Descriptions.Item>
+                  <Descriptions.Item label="Faculty">{card.faculty}</Descriptions.Item>
+                </Descriptions>
+              </Col>
+            </Row>
+          </Card>
+          <Divider orientation="center" plain />
+        </React.Fragment>
       ))}
-
     </>
   );
 }
